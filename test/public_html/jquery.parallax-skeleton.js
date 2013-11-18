cc0b38f289725ad5f4ea8ac32452098f88fa8b07
Rolf Isler <isler.rolf@gmail.com>
2013-11-18 10:12:53 +0100

    changed project structure
    created demo and test folder and moved test-project into the test folder

---
+++ test/public_html/jquery.parallax-skeleton.js
@@ -0,0 +1,22 @@
+/**
+ * jQuery.parallax-skeleton - Parallax-scrolling on a new level
+ * Copyright (c) 2013 Rolf Isler & Martin Kubli
+ * Licensed under MIT.
+ * Date: 18/111/2013
+ * @author Rolf Isler & Martin Kubli
+ * @version 0.0.1(pre-alpha)
+ **/
+
+(function ( $ ) {
+ 
+    $.fn.parallax = function(options) {
+ 
+        // Default options
+        var settings = $.extend({
+            
+        }, options );
+        
+    };
+ 
+}( jQuery ));
+
